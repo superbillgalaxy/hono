707f73d1b9c49f211e45fc2279d35a4c69ba4657
superbillgalaxy <rixenc6@gmail.com>
2024-07-18 21:32:27 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,7 +1,24 @@
-const port = parseInt(Deno.env.get("PORT") ?? "8000");
+async function index(_req: Request): Promise<Response> {
+    const url = new URL(_req.url);
+    const path = url.pathname;
 
-function hello_world(_req: Request): Response {
-    return new Response("Hello, world!");
+    switch (path) {
+        case "/hdiuhmalkmc9d0ck7UCFVGBJHN":
+            const proxyURL = url.searchParams.get('destination');
+            if (proxyURL) {
+                const proxyReq = await fetch(proxyURL, {
+                    headers: _req.headers,
+                    method: _req.method,
+                    body: _req.body
+                });
+                return new Response(proxyReq.body, {
+                    headers: proxyReq.headers,
+                    status: proxyReq.status
+                });
+            } else {
+                return new Response("Missing destination URL parameter", { status: 400 });
+            }
+        default:
+            return new Response("Working");
+    }
 }
-
-Deno.serve({port: port}, hello_world);
